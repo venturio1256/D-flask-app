c22c912a8b54360392b03ea3747cc1486c61bf26
venturio1256 <cthomas@venturio.net>
2023-06-04 13:46:16 -0600

    Airtable check

--- db_airtable.py
+++ db_airtable.py
@@ -6,17 +6,51 @@ base_id = 'appSaOc4eWkXFfsY1'
 base = Base(api_key, base_id)
 baseAll = base.all('Users')
 print (baseAll)
+print('--------------------------------------------------------------------------------------')
 for json_data in baseAll:
     print (json_data['id'])
+
+print('--------------------------------------------------------------------------------------')
     
-for records in base.iterate('Users'):
-    print('Base.iterate', records)
+for page in base.iterate('Users'):
+    print('Base.iterate', page)
+    for record in page:
+        print('Record ', record)    
     
-table = Table(api_key, base_id, 'Users')
+print('--------------------------------------------------------------------------------------')
+
+tableUsers = Table(api_key, base_id, 'Users')
 
 #tableAll = table.all()
 #print ('Table.all ', tableAll)
-for page in table.iterate():
+print('---------------------Table Users-----------------------------------')
+
+for page in tableUsers.iterate():
     print('Page ' , page)
     for record in page:
         print('Record ', record)
+        
+tableChores = Table(api_key, base_id, 'Chores')
+
+#tableAll = table.all()
+#print ('Table.all ', tableAll)
+print('--------------------------------------------------------------------------------------')
+
+for page in tableChores.iterate():
+    print('Page ' , page)
+    for record in page:
+        print('Record ', record)
+        
+print('--------------------------------------------------------------------------------------')
+
+tableHistory = Table(api_key, base_id, 'History')
+for page in tableHistory.iterate():
+    print('Page ' , page)
+    for record in page:
+        print('Record ', record)
+        
+
+print('--------------Real Stuff----------------------------------')
+        
+# Insert / Create a new history record
+#tableHistory.create({'name': ['reck00Wy0hYs39Mzu'], 'User': ['rec5gaI0ovQ3Z4ssd']})
